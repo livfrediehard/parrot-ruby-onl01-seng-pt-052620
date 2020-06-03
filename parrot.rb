d6a214a5b77c68e4601ef62cc8a7dc74fd5a85d5
Brian Palmer <bpalmer710@gmail.com>
2020-06-03 14:07:06 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squak!")
-  puts "Squawk!"
+  puts "#string"
   return "Pretty bird!"
   "Squawk!"
 end
